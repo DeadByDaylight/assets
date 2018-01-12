dfc1fe936811302ae9702ba9f25c6a0e9a012050
NeverBehave <luoxinhao77@gmail.com>
2018-01-12 22:02:11 +0800

    Create loadcommit.js

---
+++ loadcommit.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+  /** CONFIGURATION VARIABLES **/
+  var commentitUsername = 'NeverBehave';
+  var commentitRepo = 'NeverBehave/NeverBehave.github.io';
+  var commentitPath = '{{ page.path }}';
+
+  /** DON'T EDIT FOLLOWING LINES **/
+  (function() {
+      var commentit = document.createElement('script');
+      commentit.type = 'text/javascript';
+      commentit.async = true;
+      commentit.src = 'https://cdn.jsdelivr.net/gh/DeadByDaylight/assets@1.0/commitit.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(commentit);
+  })();
+</script>
